a21c3ffbb25959b02a886e14b4bb7d0a4b3b0ad4
jvsiilva <109394087+jvsiilva@users.noreply.github.com>
2022-07-28 22:18:23 -0300

    Atualização

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log("Olá");
+function revelarResposta() {
+  var resposta = document.querySelector("#resposta");
+  resposta.classList.toggle("blur");
+}
+
+function proximaPergunta(proximaPergunta) {
+  var pergunta = document.querySelector("#cardContainer");
+  pergunta.innerHTML = "";
+
+  var cardDiv = document.createElement("div");
+
+  cardDiv.classList.add("card");
+
+  console.log(proximaPergunta);
+  cardDiv.innerHTML = `
+<div class="card-cabecalho centralizar">
+            <!--Card da PERGUNTA -->
+            <h1 class="card-titulo">O que é ${proximaPergunta.title}?</h1>
+          </div>
+          <div id="resposta" class="card-conteudo blur">
+            <!--Card da RESPOSTA -->
+            <p>
+              ${proximaPergunta.description}
+            </p>
+          </div>
+`;
+  pergunta.appendChild(cardDiv);
+}
+function buscarInfo() {
+  fetch("https://flash.quickstaart.com/random")
+    .then(function (resultado) {
+      return resultado.json();
+    })
+    .then(function (resultadoJson) {
+      proximaPergunta(resultadoJson);
+    });
+}
+
+window.addEventListener("load", buscarInfo);
